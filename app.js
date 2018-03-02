b3382fc78d08171f2fad3f9526c6db6f7244b594
Zach Izham <izhamsen@hotmail.com>
2018-03-02 16:03:26 +0100

    Added user registration with passport and brcypt functionality

--- app.js
+++ app.js
@@ -93,7 +93,9 @@ app.get('/', function(req, res){
 
 // Route Files
 let articles = require('./routes/articles');
+let users = require('./routes/users');
 app.use('/articles', articles);
+app.use('/users', users);
 
 app.listen(3000, function(){
   console.log('Server started on port 3000...')
